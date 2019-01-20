183ea6252b4a0f3ee48179f857f2762c2e14d3c0
Warlander <mackordian@gmail.com>
2019-01-20 21:46:21 +0100

    Implemented lists and list elements, UI prototype of layout selection

--- Assets/Scripts/Gui/GuiManager.cs
+++ Assets/Scripts/Gui/GuiManager.cs
@@ -13,6 +13,9 @@ namespace Warlander.Deedplanner.Gui
         [SerializeField]
         private Window windowPrefab;
 
+        [SerializeField]
+        private UnityList LayoutsList;
+
         private void Awake()
         {
             if (Instance)
@@ -22,6 +25,13 @@ namespace Warlander.Deedplanner.Gui
             }
 
             Instance = this;
+
+            LayoutsList.Add("Single view");
+            LayoutsList.Add("Two horizontal views");
+            LayoutsList.Add("Two vertical views");
+            LayoutsList.Add("Large view on top and two small on bottom");
+            LayoutsList.Add("Large view on bottom and two small on top");
+            LayoutsList.Add("Four small views");
         }
 
         public Window CreateWindow(string title, RectTransform content, bool closeable)
---
+++ Assets/Scripts/Gui/SimpleUnityListElement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleUnityListElement : UnityListElement
+{
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+    [SerializeField]
+    private Toggle toggle;
+
+    private object value;
+
+    public override object Value {
+        get {
+            return value;
+        }
+        set {
+            this.value = value;
+            text.SetText(value.ToString());
+        }
+    }
+
+    public override Toggle Toggle {
+        get {
+            return toggle;
+        }
+    }
+}
