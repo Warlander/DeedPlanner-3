97ae695ba06d3d052e7fa4d0785d44d241197d33
Warlander <mackordian@gmail.com>
2019-01-22 14:40:29 +0100

    Layout select uses grid instead of list, added list events

--- Assets/Scripts/Gui/GuiManager.cs
+++ Assets/Scripts/Gui/GuiManager.cs
@@ -25,13 +25,6 @@ namespace Warlander.Deedplanner.Gui
             }
 
             Instance = this;
-
-            LayoutsList.Add("Single view");
-            LayoutsList.Add("Two horizontal views");
-            LayoutsList.Add("Two vertical views");
-            LayoutsList.Add("Large view on top and two small on bottom");
-            LayoutsList.Add("Large view on bottom and two small on top");
-            LayoutsList.Add("Four small views");
         }
 
         public Window CreateWindow(string title, RectTransform content, bool closeable)
