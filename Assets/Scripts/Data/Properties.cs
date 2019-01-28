813fd5e3e81b6bd18c5a49c6131a73aa52589b6e
Warlander <mackordian@gmail.com>
2019-01-28 22:21:35 +0100

    Added dynamic height/camera mode toggles switching

---
+++ Assets/Scripts/Data/Properties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warlander.Deedplanner.Logic
+{
+    public static class Properties
+    {
+
+        public static int FieldOfView = 60;
+
+        public static float FppMouseSensitivity = 0.5f;
+        public static float FppMovementSpeed = 16f;
+
+        public static float TopMouseSensitivity = 0.5f;
+        public static float TopMovementSpeed = 16f;
+
+        public static float IsoMouseSensitivity = 0.5f;
+        public static float IsoMovementSpeed = 16f;
+
+    }
+}
---
+++ Assets/Scripts/Logic/CameraModeReference.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Warlander.Deedplanner.Logic
+{
+    public class CameraModeReference : MonoBehaviour
+    {
+
+        public CameraMode CameraMode;
+
+    }
+}
