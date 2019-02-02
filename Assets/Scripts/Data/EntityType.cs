2c1b34d1f5d0d675232c0577d45dc155153ef2db
Warlander <mackordian@gmail.com>
2019-02-02 22:28:24 +0100

    Added EntityData and removed unnecessary standard assets

---
+++ Assets/Scripts/Data/EntityType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warlander.Deedplanner.Data
+{
+    public enum EntityType
+    {
+        FLOORROOF, HWALL, HFENCE, HBORDER, VWALL, VFENCE, VBORDER, OBJECT, CAVE, LABEL
+    }
+}
---
+++ Assets/Scripts/Data/ITileEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warlander.Deedplanner.Utils;
+
+namespace Warlander.Deedplanner.Data
+{
+
+    public interface ITileEntity : IXMLSerializable
+    {
+
+        Materials Materials { get; }
+
+    }
+
+}
