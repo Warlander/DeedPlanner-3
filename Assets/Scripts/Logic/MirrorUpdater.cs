8210db713e4fc9c1469c5153c94574f047e01ef8
Warlander <mackordian@gmail.com>
2019-03-24 04:16:26 +0100

    Added stubs of all updaters, animals merged with objects

---
+++ Assets/Scripts/Logic/MirrorUpdater.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Warlander.Deedplanner.Data;
+using Warlander.Deedplanner.Gui;
+
+namespace Warlander.Deedplanner.Logic
+{
+    public class MirrorUpdater : MonoBehaviour
+    {
+
+        public void OnEnable()
+        {
+            LayoutManager.Instance.TileSelectionMode = TileSelectionMode.Nothing;
+        }
+
+        public void Update()
+        {
+            RaycastHit raycast = LayoutManager.Instance.CurrentCamera.CurrentRaycast;
+            if (raycast.transform == null)
+            {
+                return;
+            }
+
+            BasicTile tile = raycast.transform.GetComponentInParent<BasicTile>();
+            GridTile gridTile = raycast.transform.GetComponent<GridTile>();
+            
+        }
+
+    }
+}
