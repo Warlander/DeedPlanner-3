f452a716435fc90b484e984185cab3d34d66dad3
Warlander <mackordian@gmail.com>
2019-04-24 00:45:23 +0200

    Made water tables follow the camera, lowered water performance impact

---
+++ Assets/Scripts/Utils/TransformFollower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Warlander.Deedplanner.Utils
+{
+    public class TransformFollower : MonoBehaviour
+    {
+
+        public Transform FollowedTransform;
+        public bool FollowX;
+        public bool FollowY;
+        public bool FollowZ;
+
+        private void Update()
+        {
+            if (!FollowedTransform)
+            {
+                return;
+            }
+
+            float x = FollowX ? FollowedTransform.position.x : transform.position.x;
+            float y = FollowY ? FollowedTransform.position.y : transform.position.y;
+            float z = FollowZ ? FollowedTransform.position.z : transform.position.z;
+            transform.position = new Vector3(x, y, z);
+        }
+
+    }
+}
