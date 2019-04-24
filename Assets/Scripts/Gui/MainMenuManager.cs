9595f163fd81666b41de34cfd0ccfe19297505df
Warlander <mackordian@gmail.com>
2019-04-25 00:07:10 +0200

    Added main menu manager, GUI bindings and fullscreen/quit button code

---
+++ Assets/Scripts/Gui/MainMenuManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Warlander.Deedplanner.Gui
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Button fullscreenButton = null;
+        [SerializeField]
+        private Button quitButton = null;
+
+        private void Start()
+        {
+            bool mobile = Application.isMobilePlatform;
+            bool web = Application.platform == RuntimePlatform.WebGLPlayer;
+
+            if (mobile || web)
+            {
+                fullscreenButton.gameObject.SetActive(false);
+                quitButton.gameObject.SetActive(false);
+            }
+        }
+
+        public void OnResizeMap()
+        {
+
+        }
+
+        public void OnClearMap()
+        {
+
+        }
+
+        public void OnSaveMap()
+        {
+
+        }
+
+        public void OnLoadMap()
+        {
+
+        }
+
+        public void OnGraphicsSettings()
+        {
+
+        }
+
+        public void OnInputSettings()
+        {
+
+        }
+
+        public void OnToggleFullscreen()
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+
+        public void OnQuit()
+        {
+            Application.Quit();
+        }
+
+    }
+}
