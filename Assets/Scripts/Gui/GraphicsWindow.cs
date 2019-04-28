3d1d68d117ab340028c4c9287db45f69eb8bafd8
Warlander <mackordian@gmail.com>
2019-04-28 12:56:03 +0200

    Added saving/loading of water quality, properties framework

---
+++ Assets/Scripts/Gui/GraphicsWindow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Warlander.Deedplanner.Gui
+{
+    public class GraphicsWindow : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Toggle simpleWaterToggle;
+        [SerializeField]
+        private Toggle highWaterToggle;
+        [SerializeField]
+        private Toggle ultraWaterToggle;
+
+        private void OnEnable()
+        {
+            ResetState();
+            ApplyProperties();
+        }
+
+        private void ResetState()
+        {
+            simpleWaterToggle.isOn = false;
+            highWaterToggle.isOn = false;
+            ultraWaterToggle.isOn = false;
+        }
+
+        private void ApplyProperties()
+        {
+            WaterQuality waterQuality = Properties.WaterQuality;
+            switch (waterQuality)
+            {
+                case WaterQuality.SIMPLE:
+                    simpleWaterToggle.isOn = true;
+                    break;
+                case WaterQuality.HIGH:
+                    highWaterToggle.isOn = true;
+                    break;
+                case WaterQuality.ULTRA:
+                    ultraWaterToggle.isOn = true;
+                    break;
+            }
+        }
+
+        public void OnSaveButton()
+        {
+            gameObject.SetActive(false);
+            SaveProperties();
+        }
+
+        private void SaveProperties()
+        {
+            if (simpleWaterToggle.isOn)
+            {
+                Properties.WaterQuality = WaterQuality.SIMPLE;
+            }
+            else if (highWaterToggle.isOn)
+            {
+                Properties.WaterQuality = WaterQuality.HIGH;
+            }
+            else if (ultraWaterToggle.isOn)
+            {
+                Properties.WaterQuality = WaterQuality.ULTRA;
+            }
+
+            Properties.SaveProperties();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Gui/WaterQuality.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warlander.Deedplanner.Gui
+{
+    public enum WaterQuality
+    {
+        SIMPLE = 0,
+        HIGH = 1,
+        ULTRA = 2
+    }
+}
