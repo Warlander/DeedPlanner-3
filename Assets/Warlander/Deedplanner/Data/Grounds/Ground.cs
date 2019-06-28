22c17a6bf0a77c4802f5cf2c4f472cd0784c0266
Warlander <mackordian@gmail.com>
2019-06-29 01:01:17 +0200

    Added undo/redo for grounds

--- Assets/Warlander/Deedplanner/Data/Grounds/Ground.cs
+++ Assets/Warlander/Deedplanner/Data/Grounds/Ground.cs
@@ -1,6 +1,7 @@
 ﻿using System.Xml;
 using UnityEngine;
 using Warlander.Deedplanner.Logic;
+using Warlander.Deedplanner.Utils;
 
 namespace Warlander.Deedplanner.Data.Grounds
 {
@@ -9,7 +10,7 @@ namespace Warlander.Deedplanner.Data.Grounds
 
         private Tile tile;
         private GroundData data;
-        private RoadDirection roadDirection;
+        private RoadDirection roadDirection = RoadDirection.Center;
 
         private MeshRenderer meshRenderer;
         public MeshCollider Collider { get; private set; }
@@ -19,57 +20,12 @@ namespace Warlander.Deedplanner.Data.Grounds
 
         public GroundData Data {
             get => data;
-            set {
-                data = value;
-                RoadDirection = roadDirection;
-            }
+            set => tile.Map.CommandManager.AddToActionAndExecute(new GroundDataChangeCommand(this, data, value));
         }
 
         public RoadDirection RoadDirection {
             get => RoadDirection;
-            set {
-                roadDirection = value;
-
-                Material[] materials = new Material[4];
-                if (roadDirection == RoadDirection.Center)
-                {
-                    for (int i = 0; i < 4; i++)
-                    {
-                        materials[i] = Data.Tex3d.Material;
-                    }
-                }
-                else
-                {
-                    Material matW = GameManager.Instance.Map.GetRelativeTile(tile, -1, 0)?.Ground.Data.Tex3d.Material;
-                    Material matE = GameManager.Instance.Map.GetRelativeTile(tile, 1, 0)?.Ground.Data.Tex3d.Material;
-                    Material matS = GameManager.Instance.Map.GetRelativeTile(tile, 0, -1)?.Ground.Data.Tex3d.Material;
-                    Material matN = GameManager.Instance.Map.GetRelativeTile(tile, 0, 1)?.Ground.Data.Tex3d.Material;
-
-                    if (roadDirection == RoadDirection.NW || roadDirection == RoadDirection.SW || !matW)
-                    {
-                        matW = Data.Tex3d.Material;
-                    }
-                    if (roadDirection == RoadDirection.NE || roadDirection == RoadDirection.SE || !matE)
-                    {
-                        matE = Data.Tex3d.Material;
-                    }
-                    if (roadDirection == RoadDirection.SW || roadDirection == RoadDirection.SE || !matS)
-                    {
-                        matS = Data.Tex3d.Material;
-                    }
-                    if (roadDirection == RoadDirection.NW || roadDirection == RoadDirection.NE || !matN)
-                    {
-                        matN = Data.Tex3d.Material;
-                    }
-
-                    materials[0] = matW;
-                    materials[1] = matN;
-                    materials[2] = matE;
-                    materials[3] = matS;
-                }
-
-                meshRenderer.materials = materials;
-            }
+            set => tile.Map.CommandManager.AddToActionAndExecute(new RoadDirectionChangeCommand(this, roadDirection, value));
         }
 
         public void Initialize(Tile tile, GroundData data, Mesh mesh)
@@ -105,6 +61,49 @@ namespace Warlander.Deedplanner.Data.Grounds
             meshRenderer.sharedMaterials = materials;
         }
 
+        private void RefreshState()
+        {
+            Material[] materials = new Material[4];
+            if (roadDirection == RoadDirection.Center)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    materials[i] = Data.Tex3d.Material;
+                }
+            }
+            else
+            {
+                Material matW = GameManager.Instance.Map.GetRelativeTile(tile, -1, 0)?.Ground.Data.Tex3d.Material;
+                Material matE = GameManager.Instance.Map.GetRelativeTile(tile, 1, 0)?.Ground.Data.Tex3d.Material;
+                Material matS = GameManager.Instance.Map.GetRelativeTile(tile, 0, -1)?.Ground.Data.Tex3d.Material;
+                Material matN = GameManager.Instance.Map.GetRelativeTile(tile, 0, 1)?.Ground.Data.Tex3d.Material;
+
+                if (roadDirection == RoadDirection.NW || roadDirection == RoadDirection.SW || !matW)
+                {
+                    matW = Data.Tex3d.Material;
+                }
+                if (roadDirection == RoadDirection.NE || roadDirection == RoadDirection.SE || !matE)
+                {
+                    matE = Data.Tex3d.Material;
+                }
+                if (roadDirection == RoadDirection.SW || roadDirection == RoadDirection.SE || !matS)
+                {
+                    matS = Data.Tex3d.Material;
+                }
+                if (roadDirection == RoadDirection.NW || roadDirection == RoadDirection.NE || !matN)
+                {
+                    matN = Data.Tex3d.Material;
+                }
+
+                materials[0] = matW;
+                materials[1] = matN;
+                materials[2] = matE;
+                materials[3] = matS;
+            }
+
+            meshRenderer.materials = materials;
+        }
+
         public override void Serialize(XmlDocument document, XmlElement localRoot)
         {
             localRoot.SetAttribute("id", data.ShortName);
@@ -136,6 +135,86 @@ namespace Warlander.Deedplanner.Data.Grounds
                 default:
                     RoadDirection = RoadDirection.Center;
                     break;
+            }
+        }
+
+        private class GroundDataChangeCommand : IReversibleCommand
+        {
+
+            private Ground ground;
+            private GroundData oldData;
+            private GroundData newData;
+            
+            public GroundDataChangeCommand(Ground ground, GroundData oldData, GroundData newData)
+            {
+                this.ground = ground;
+                this.oldData = oldData;
+                this.newData = newData;
+            }
+            
+            public void Execute()
+            {
+                if (newData)
+                {
+                    ground.data = newData;
+                    ground.RefreshState();
+                }
+            }
+
+            public void Undo()
+            {
+                if (oldData)
+                {
+                    ground.data = oldData;
+                    ground.RefreshState();
+                }
+            }
+
+            public void DisposeUndo()
+            {
+                // no operation needed
+            }
+
+            public void DisposeRedo()
+            {
+                // no operation needed
+            }
+        }
+        
+        private class RoadDirectionChangeCommand : IReversibleCommand
+        {
+
+            private Ground ground;
+            private RoadDirection oldDirection;
+            private RoadDirection newDirection;
+            
+            public RoadDirectionChangeCommand(Ground ground, RoadDirection oldDirection, RoadDirection newDirection)
+            {
+                this.ground = ground;
+                this.oldDirection = oldDirection;
+                this.newDirection = newDirection;
+            }
+            
+            public void Execute()
+            {
+                ground.roadDirection = newDirection;
+                ground.RefreshState();
+            }
+
+            public void Undo()
+            {
+                ground.roadDirection = oldDirection;
+                ground.RefreshState();
+            }
+
+            public void DisposeUndo()
+            {
+                // no operation needed
+            }
+
+            public void DisposeRedo()
+            {
+                // no operation needed
             }
         }
 
