a22ed7712b309c94a05d6852fb224de88a8865dc
Warlander <mackordian@gmail.com>
2019-07-02 01:56:26 +0200

    Fixed depth problems on orthographic cameras

--- Assets/Warlander/Deedplanner/Gui/ResizableRenderTexture.cs
+++ Assets/Warlander/Deedplanner/Gui/ResizableRenderTexture.cs
@@ -29,7 +29,7 @@ namespace Warlander.Deedplanner.Gui
                 return;
             }
 
-            RenderTexture renderTexture = new RenderTexture((int) width, (int) height, 16, RenderTextureFormat.RGB111110Float);
+            RenderTexture renderTexture = new RenderTexture((int) width, (int) height, 16, RenderTextureFormat.ARGB64);
             rawImage.texture = renderTexture;
 
             if (renderCamera)
