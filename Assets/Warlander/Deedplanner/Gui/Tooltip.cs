27e7fa36656565a1e53d373e7c12c49f8dfb37fe
Warlander <mackordian@gmail.com>
2019-07-14 03:35:04 +0200

    Added framework for tooltips

---
+++ Assets/Warlander/Deedplanner/Gui/Tooltip.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+namespace Warlander.Deedplanner.Gui
+{
+    public class Tooltip : MonoBehaviour
+    {
+
+        [SerializeField] private TMP_Text text;
+
+        private RectTransform rectTransform;
+
+        public string Value
+        {
+            get => text.text;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    gameObject.SetActive(false);
+                }
+                
+                gameObject.SetActive(true);
+                
+            }
+        }
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+        
+        private void Update()
+        {
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 cursorCorrection = new Vector2(0, -20);
+            rectTransform.anchoredPosition = mousePos + cursorCorrection;
+        }
+
+    }
+}
