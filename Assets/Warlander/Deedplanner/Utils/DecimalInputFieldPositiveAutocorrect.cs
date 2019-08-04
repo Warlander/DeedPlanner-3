a173f69a97eb71f9a73632a232cf0c91ee320a00
Warlander <mackordian@gmail.com>
2019-08-04 03:18:23 +0200

    Added drag sensitivity options to height editing

---
+++ Assets/Warlander/Deedplanner/Utils/DecimalInputFieldPositiveAutocorrect.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace Warlander.Deedplanner.Utils
+{
+    [RequireComponent((typeof(TMP_InputField)))]
+    public class DecimalInputFieldPositiveAutocorrect : MonoBehaviour
+    {
+        
+        private TMP_InputField inputField;
+        
+        private void Awake()
+        {
+            inputField = GetComponent<TMP_InputField>();
+            inputField.onValueChanged.AddListener(OnTextChanged);
+        }
+        
+        private void OnTextChanged(string content)
+        {
+            string text = content.Replace("-", "");
+            
+            inputField.text = text;
+        }
+    }
+}
