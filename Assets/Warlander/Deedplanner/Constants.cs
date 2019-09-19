a358891c6399b26b8adae4c201573ac805e14cca
Warlander <mackordian@gmail.com>
2019-09-19 02:33:02 +0200

    Version bump to 3.0.3

--- Assets/Warlander/Deedplanner/Constants.cs
+++ Assets/Warlander/Deedplanner/Constants.cs
@@ -4,7 +4,7 @@
     public static class Constants
     {
 
-        public const string VersionString = "3.0.2 ALPHA";
+        public const string VersionString = "3.0.3 ALPHA";
         public const string TitleString = "DeedPlanner " + VersionString;
 
         public const string WebVersionLink = "https://warlander.github.io/DeedPlanner-3-web";
