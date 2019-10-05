29dcce0d2af839aba7651cd3a5b7a1270d0d2cb4
Warlander <mackordian@gmail.com>
2019-10-06 01:16:29 +0200

    Made OverlayMesh a prefab instead of new GameObject

--- Assets/Warlander/Deedplanner/Data/OverlayMesh.cs
+++ Assets/Warlander/Deedplanner/Data/OverlayMesh.cs
@@ -3,46 +3,20 @@ using Warlander.Deedplanner.Logic;
 
 namespace Warlander.Deedplanner.Data
 {
+    [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter), typeof(MeshCollider))]
     public class OverlayMesh : MonoBehaviour
     {
         
-        private static Material gridMaterial;
-        
         private MeshFilter meshFilter;
-        private MeshRenderer meshRenderer;
         private MeshCollider meshCollider;
 
         public void Initialize(Mesh targetMesh)
         {
             gameObject.layer = LayerMasks.TileLayer;
-            if (!gridMaterial)
-            {
-                gridMaterial = new Material(Shader.Find("Standard"));
-                gridMaterial.SetOverrideTag("RenderType", "TransparentCutout");
-                gridMaterial.EnableKeyword("_ALPHATEST_ON");
-                gridMaterial.renderQueue = 2450;
-                gridMaterial.color = new Color(0, 0, 0, 0);
-            }
-            
+
             meshFilter = GetComponent<MeshFilter>();
-            if (!meshFilter)
-            {
-                meshFilter = gameObject.AddComponent<MeshFilter>();
-            }
-
-            meshRenderer = GetComponent<MeshRenderer>();
-            if (!meshRenderer)
-            {
-                meshRenderer = gameObject.AddComponent<MeshRenderer>();
-            }
-            
             meshCollider = GetComponent<MeshCollider>();
-            if (!meshCollider)
-            {
-                meshCollider = gameObject.AddComponent<MeshCollider>();
-            }
-
-            meshRenderer.sharedMaterial = gridMaterial;
+            
             meshFilter.sharedMesh = targetMesh;
             meshCollider.sharedMesh = targetMesh;
         }
