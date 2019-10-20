257248252dc53ad4abd459903227bcafbf446a9e
Warlander <mackordian@gmail.com>
2019-10-21 00:30:24 +0200

    Merged MenuUpdater with MainMenuManager, moved donation to main menu

--- Assets/Warlander/Deedplanner/Updaters/MenuUpdater.cs
+++ Assets/Warlander/Deedplanner/Updaters/MenuUpdater.cs
@@ -1,6 +1,7 @@
 using Steamworks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Warlander.Deedplanner.Gui;
 using Warlander.Deedplanner.Logic;
 
@@ -9,7 +10,29 @@ namespace Warlander.Deedplanner.Updaters
     public class MenuUpdater : MonoBehaviour
     {
 
+        [SerializeField] private Button fullscreenButton = null;
+        [SerializeField] private Button quitButton = null;
+        
         [SerializeField] private TMP_Text steamConnectionText;
+        [SerializeField] private TMP_Text versionText = null;
+        
+        private void Start()
+        {
+            bool mobile = Application.isMobilePlatform;
+            bool web = Application.platform == RuntimePlatform.WebGLPlayer;
+
+            if (mobile || web)
+            {
+                quitButton.gameObject.SetActive(false);
+            }
+
+            if (mobile)
+            {
+                fullscreenButton.gameObject.SetActive(false);
+            }
+
+            versionText.text = Constants.TitleString;
+        }
         
         private void OnEnable()
         {
@@ -21,6 +44,71 @@ namespace Warlander.Deedplanner.Updaters
             {
                 steamConnectionText.text = "Connected to Steam as " + SteamFriends.GetPersonaName();
             }
+        }
+
+        public void OnResizeMap()
+        {
+            GuiManager.Instance.ResizeMapWindow.gameObject.SetActive(true);
+        }
+
+        public void OnClearMap()
+        {
+            GuiManager.Instance.ClearMapWindow.gameObject.SetActive(true);
+        }
+
+        public void OnSaveMap()
+        {
+            GuiManager.Instance.SaveMapWindow.gameObject.SetActive(true);
+        }
+
+        public void OnLoadMap()
+        {
+            GuiManager.Instance.LoadMapWindow.gameObject.SetActive(true);
+        }
+
+        public void OnGraphicsSettings()
+        {
+            GuiManager.Instance.GraphicsSettingsWindow.gameObject.SetActive(true);
+        }
+
+        public void OnInputSettings()
+        {
+            GuiManager.Instance.InputSettingsWindow.gameObject.SetActive(true);
+        }
+
+        public void OnAbout()
+        {
+            GuiManager.Instance.AboutWindow.gameObject.SetActive(true);
+        }
+
+        public void OnToggleFullscreen()
+        {
+            if (Screen.fullScreen)
+            {
+                Screen.fullScreen = false;
+            }
+            else
+            {
+                // makes sure fullscreen mode always uses intended fullscreen window mode instead of native window or other fullscreen mode saved in user settings
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
+            }
+        }
+
+        public void OnQuit()
+        {
+            // TODO: add auto-saving before quit logic
+            Properties.Instance.SaveProperties();
+            Application.Quit();
+        }
+
+        public void OnPatreon()
+        {
+            Application.OpenURL("https://www.patreon.com/warlander");
+        }
+
+        public void OnPaypal()
+        {
+            Application.OpenURL("https://www.paypal.me/MCyranowicz/10eur");
         }
         
     }
