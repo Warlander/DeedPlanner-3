439841a09ac3c8e298fa277adde213c2b5fdd78a
Warlander <mackordian@gmail.com>
2019-10-20 23:54:20 +0200

    Added Steam connection info in main menu

--- Assets/Warlander/Deedplanner/Updaters/MenuUpdater.cs
+++ Assets/Warlander/Deedplanner/Updaters/MenuUpdater.cs
@@ -1,3 +1,5 @@
+using Steamworks;
+using TMPro;
 using UnityEngine;
 using Warlander.Deedplanner.Gui;
 using Warlander.Deedplanner.Logic;
@@ -6,10 +8,19 @@ namespace Warlander.Deedplanner.Updaters
 {
     public class MenuUpdater : MonoBehaviour
     {
+
+        [SerializeField] private TMP_Text steamConnectionText;
         
         private void OnEnable()
         {
             LayoutManager.Instance.TileSelectionMode = TileSelectionMode.Nothing;
+
+            bool connectedToSteam = SteamManager.ConnectedToSteam;
+            steamConnectionText.gameObject.SetActive(connectedToSteam);
+            if (connectedToSteam)
+            {
+                steamConnectionText.text = "Connected to Steam as " + SteamFriends.GetPersonaName();
+            }
         }
         
     }
