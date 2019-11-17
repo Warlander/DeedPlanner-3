919aef5ff7707338e297d943249b8170eac24bbd
Warlander <mackordian@gmail.com>
2019-11-18 00:02:39 +0100

    Added way to access tiles and rooms in building

--- Assets/Warlander/Deedplanner/Data/Summary/Building.cs
+++ Assets/Warlander/Deedplanner/Data/Summary/Building.cs
@@ -5,6 +5,8 @@ namespace Warlander.Deedplanner.Data.Summary
     public class Building
     {
         private List<Room> rooms;
+        
+        public IReadOnlyList<Room> Rooms => rooms.AsReadOnly();
 
         public Building(List<Room> newRooms)
         {
