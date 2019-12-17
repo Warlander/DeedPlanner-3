e0a313a483ed5fedcb999699bc304a9fbf4f7b75
Warlander <mackordian@gmail.com>
2019-12-18 00:04:12 +0100

    Reworked windows system, added animation support

--- Assets/Warlander/Deedplanner/Gui/Widgets/Window.cs
+++ Assets/Warlander/Deedplanner/Gui/Widgets/Window.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace Warlander.Deedplanner.Gui.Widgets
@@ -7,12 +8,10 @@ namespace Warlander.Deedplanner.Gui.Widgets
     public class Window : MonoBehaviour
     {
 
-        [SerializeField]
-        private GameObject closeButton = null;
-        [SerializeField]
-        private RectTransform contentAnchor = null;
-        [SerializeField]
-        private TextMeshProUGUI titleGui = null;
+        [SerializeField] private CanvasGroup canvasGroup = null;
+        [SerializeField] private GameObject closeButton = null;
+        [SerializeField] private RectTransform contentAnchor = null;
+        [SerializeField] private TextMeshProUGUI titleGui = null;
 
         public bool CloseButtonVisible {
             get => closeButton.activeInHierarchy;
@@ -30,6 +29,32 @@ namespace Warlander.Deedplanner.Gui.Widgets
         public string Title {
             get => titleGui.text;
             set => titleGui.text = value;
+        }
+
+        private bool windowShown = false;
+
+        public void ShowWindow()
+        {
+            if (windowShown)
+            {
+                return;
+            }
+
+            windowShown = true;
+            gameObject.SetActive(true);
+            canvasGroup.alpha = 0;
+            canvasGroup.DOFade(1, 0.15f).SetEase(Ease.Linear);
+        }
+
+        public void HideWindow()
+        {
+            if (!windowShown)
+            {
+                return;
+            }
+
+            windowShown = false;
+            canvasGroup.DOFade(0, 0.15f).SetEase(Ease.Linear).OnComplete(() => gameObject.SetActive(false));
         }
 
     }
