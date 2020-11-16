0b9598595f4d0630723c6fd7cb9ff2fe50826ecf
Maciej Cyranowicz <mackordian@gmail.com>
2020-11-16 01:37:51 +0100

    Added debug map selection, made Bridge World default debug map

--- Assets/Warlander/Deedplanner/Logic/DebugManager.cs
+++ Assets/Warlander/Deedplanner/Logic/DebugManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using SFB;
 using UnityEngine;
 using Warlander.Deedplanner.Data;
@@ -14,7 +16,7 @@ namespace Warlander.Deedplanner.Logic
     {
         public static DebugManager Instance { get; private set; }
         
-        [SerializeField] private bool loadTestMap = false;
+        [SerializeField] private TestMap testMap = TestMap.Warland;
         [SerializeField] private bool preloadAllDecorations = false;
 
         [SerializeField] private bool overrideStartingTileSelectionMode = false;
@@ -22,7 +24,8 @@ namespace Warlander.Deedplanner.Logic
         
         [SerializeField] private bool drawDebugPlaneLines = false;
 
-        public bool LoadTestMap => loadTestMap;
+        public bool ShouldLoadTestMap => testMap != TestMap.None;
+        public string TestMapPath => GetTestMapPath(testMap);
 
         public DebugManager()
         {
@@ -94,7 +97,31 @@ namespace Warlander.Deedplanner.Logic
 
         private void OnModelLoaded(GameObject model)
         {
-            
+            // Do nothing, this is test of model loading system and we do nothing extra with loaded model.
+        }
+
+        private string GetTestMapPath(TestMap map)
+        {
+            switch (map)
+            {
+                case TestMap.None:
+                    return null;
+                case TestMap.Warland:
+                    return Path.Combine(Application.streamingAssetsPath, "./Special/Maps/Test Map.MAP");
+                case TestMap.Roofs:
+                    return Path.Combine(Application.streamingAssetsPath, "./Special/Maps/Roof World.MAP");
+                case TestMap.Bridges:
+                    return Path.Combine(Application.streamingAssetsPath, "./Special/Maps/Bridge World.MAP");
+                default:
+                    Debug.LogWarning($"No asset path mapping found for test map {map}");
+                    return null;
+            }
+        }
+
+        [Serializable]
+        public enum TestMap
+        {
+            None, Warland, Roofs, Bridges
         }
     }
 }
