5895406c724679b1787caca458d30f70e4f8d0db
Maciej Cyranowicz <mackordian@gmail.com>
2021-01-29 23:58:48 +0100

    Added basic support for Windows file drag and drop to load maps

---
+++ Assets/Warlander/Deedplanner/Logic/FileDragManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using B83.Win32;
+using UnityEngine;
+
+namespace Warlander.Deedplanner.Logic
+{
+    public class FileDragManager : MonoBehaviour
+    {
+        #if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        private void OnEnable()
+        {
+            UnityDragAndDropHook.InstallHook();
+            UnityDragAndDropHook.OnDroppedFiles += OnFileDropped;
+        }
+
+        private void OnFileDropped(List<string> files, POINT point)
+        {
+            if (files.Count != 1 || !files[0].EndsWith(".MAP"))
+            {
+                return;
+            }
+            
+            GameManager.Instance.LoadMap(File.ReadAllText(files[0]));
+        }
+        
+        private void OnDisable()
+        {
+            UnityDragAndDropHook.UninstallHook();
+            UnityDragAndDropHook.OnDroppedFiles -= OnFileDropped;
+        }
+        #else
+        public void Awake()
+        {
+            // Immediately destroy the manager on other platforms than Windows.
+            Destroy(gameObject);
+        }
+        #endif
+    }
+}
