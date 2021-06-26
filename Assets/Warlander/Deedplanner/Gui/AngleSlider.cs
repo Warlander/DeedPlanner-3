31fc4efdd9ed857511c37c268ff18d3524aaba7a
Tux Solbakk <tux@krageroby.no>
2021-06-26 22:19:20 +0200

    Slider graphics, text, tooltip and prefab.

---
+++ Assets/Warlander/Deedplanner/Gui/AngleSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AngleSlider : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Light source;
+
+    Vector3 angle;
+
+    void Start()
+    {
+        angle = source.transform.localEulerAngles;
+
+        slider.onValueChanged.AddListener((value) => {
+            source.transform.localEulerAngles = new Vector3(angle.x, value, angle.z);
+        });
+    }
+}
