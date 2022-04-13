a5f5ddeadc0ec34d5d346c75b6e931a09156c6d8
jackwright <56135737+jackwright-ual@users.noreply.github.com>
2022-04-13 13:03:20 +0100

    Base implementation of Input settings purely in Unity

---
+++ Assets/Warlander/Deedplanner/Gui/InputSettingsWindow.cs
@@ -0,0 +1,77 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Warlander.Deedplanner.Gui
+{
+    public class InputSettingsWindow : MonoBehaviour
+    {
+
+        private TMP_InputField[] dummyInputs = new TMP_InputField[] { };
+        [SerializeField] private TMP_InputField dummyField = null;
+
+        private Dictionary<string, string> keyEvents = new Dictionary<string, string>(){ };
+
+        /*
+         * Move Forward R
+         * Move Backwards F
+         * Move Up E
+         * Move Down Q
+         */
+
+        private void Awake()
+        {
+            keyEvents.Add("negativeX", "Reverse");
+            keyEvents.Add("negativeY", "Downwards");
+            keyEvents.Add("positiveX", "Forward");
+            keyEvents.Add("positiveY", "Upwards");
+
+            foreach(TMP_InputField input in dummyInputs)
+            {
+                
+            }
+
+            
+        }
+
+        private void OnEnable()
+        {
+            //ResetState();
+            //ApplyProperties();
+        }
+
+
+        /* 
+         * Used to reset window state
+         */
+        private void ResetState()
+        {
+            
+        }
+
+
+        /*
+         * used when save chaegs is made
+         */
+        private void ApplyProperties()
+        {
+            
+        }
+
+        public void OnGuiScaleChanged()
+        {
+        }
+
+        public void OnSaveButton()
+        {
+            //SaveProperties();
+            //gameObject.SetActive(false);
+        }
+
+        private void SaveProperties()
+        {
+            
+        }
+    }
+}
