8ffbfb67c542fed2fa83f86520995fe9dab6e884
jackwright <56135737+jackwright-ual@users.noreply.github.com>
2022-06-27 11:57:04 +0100

    Dynamically added keybinds using Enum

--- Assets/Warlander/Deedplanner/Gui/InputSettingsWindow.cs
+++ Assets/Warlander/Deedplanner/Gui/InputSettingsWindow.cs
@@ -1,7 +1,8 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Warlander.Deedplanner.Gui
 {
@@ -9,9 +10,6 @@ namespace Warlander.Deedplanner.Gui
     {
 
         private TMP_InputField[] dummyInputs = new TMP_InputField[] { };
-
-        private Dictionary<string, string> keyEvents = new Dictionary<string, string>(){ };
-
 
         private Transform inputContentHolder;
         /*
@@ -23,26 +21,25 @@ namespace Warlander.Deedplanner.Gui
 
         private void Start()
         {
-            keyEvents.Add("negativeX", "Reverse");
-            keyEvents.Add("negativeY", "Downward");
-            keyEvents.Add("positiveX", "Forward");
-            keyEvents.Add("positiveY", "Upward");
 
             inputContentHolder = GameObject.Find("inputSettingvGroup").transform;
             GameObject dummyInput = GameObject.Find("dummyInputSettingGroup");
 
             //clone dummy input to requried fields
-            foreach (var key in keyEvents)
+            foreach (var key in Enum.GetNames(typeof(Inputs)))
             {
                 GameObject clone = Instantiate(dummyInput, inputContentHolder);
 
-                clone.name = key.Key;
+                clone.name = key;
 
-                Debug.Log(key.Value);
                 Transform tr = clone.transform;
+
+                //var iField = tr.Find("InputField (TMP)");
+                //iField.GetComponent<TMP_InputField>().OnChangeEvent.AddListener((string input) => { checkInputChange(); });
+
                 var go = tr.Find("dummyText");
                 TMP_Text t = go.GetComponent<TMP_Text>();
-                t.text = key.Value;
+                t.text = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(key.ToLower());
             }
 
             var saveButton = inputContentHolder.Find("Save Button");
@@ -57,60 +54,77 @@ namespace Warlander.Deedplanner.Gui
 
         private void Awake()
         {
-            //TMP_InputField duplicate = Instantiate(dummyField);
-
-
-
-
         }
 
+        private Dictionary<string, bool> keysDown = new Dictionary<string, bool>(){ };
+        private Dictionary<string, string> currentValues = new Dictionary<string, string>(){ };
+
+
+        //TODO: there is a problem setting text to files with update..perhaps values should be saved to a dict and set at the end of method
         public void Update()
         {
-            bool shiftDown = false;
 
-            //clone dummy input to requried fields
-            foreach (var key in keyEvents)
+
+            //TODO: Add functionality to detect touch
+
+            keysDown.Clear();
+
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
             {
-                var text = "";
+                if (Input.GetKeyDown(kcode))
+                {
+                    keysDown.Add(kcode.ToString(), true);
+                }
+            }
 
-                Transform fieldGroup = inputContentHolder.Find(key.Key).transform;
+            foreach (var key in Enum.GetNames(typeof(Inputs)))
+            {
+                
+                Transform fieldGroup = inputContentHolder.Find(key).transform;
                 var i = fieldGroup.Find("InputField (TMP)");
 
                 TMP_InputField tInput = i.GetComponent<TMP_InputField>();
 
+                tInput.text = currentValues.ContainsKey(key) ? currentValues[key] : "UNSET";
+                //tInput.textComponent.SetText(currentValues.ContainsKey(key) ? currentValues[key] : "UNSET");
+
                 if (tInput.isFocused)
                 {
-                    if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+
+                    if(Input.GetKey(KeyCode.CapsLock) || Input.GetKey(KeyCode.Numlock) && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
                     {
-                        shiftDown = true;
-                    }
-                    if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
-                    {
-                        shiftDown = false;
+                        return;
                     }
 
-                    if (Input.anyKey && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))//TODO: Add functionality to detect touch
+                    //clear field
+                    if (Input.GetKeyDown(KeyCode.Escape))
                     {
-                        if (Input.inputString != "") {
-                            Debug.Log("Key was held down for " + key.Key + " " + Input.inputString);
-                            if(shiftDown)
-                            {
-                                text = "Shift";
-                            }
-                            if(text != "")
-                            {
-                                text += " + " + Input.inputString;
-                            } else
-                            {
-                                text = Input.inputString;
-                            }
+                        currentValues.Remove(key);
+                        tInput.text = "";
+                        return;
+                    }
+
+                    //delete character
+                    if (Input.GetKeyDown(KeyCode.Backspace))
+                    {
+                        keysDown.Remove(keysDown.Keys.Last());
+                        return;
+                    }
+
+                    string newVal = String.Join("+", keysDown.Keys);
+
+                    if (currentValues.ContainsKey(key))
+                    {
+                        if (currentValues[key] != newVal)
+                        {
+                            currentValues[key] = newVal;
                         }
                     }
-
+                    else
+                    {
+                        currentValues.Add(key, String.Join("+", keysDown.Keys));
+                    }
                 }
-
-                tInput.text = text;
-
             }
         }
 
---
+++ Assets/Warlander/Deedplanner/Gui/Inputs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Warlander.Deedplanner.Gui {
+
+    public enum Inputs
+    {
+        UPWARD,
+        DOWNWARD,
+        REVERSE,
+        FORWARD
+    }
+}
