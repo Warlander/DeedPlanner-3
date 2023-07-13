623d828b5b171a5201b752dbbd0873b77f7db2b2
Maciej Cyranowicz <mackordian@gmail.com>
2023-07-14 01:25:28 +0200

    Added ProjectContext and Main Scene's SceneContext

---
+++ Assets/Warlander/Deedplanner/Installers/MainSceneInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Warlander.Deedplanner.Installers
+{
+    public class MainSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Warlander/Deedplanner/Installers/ProjectInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Warlander.Deedplanner.Installers
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
