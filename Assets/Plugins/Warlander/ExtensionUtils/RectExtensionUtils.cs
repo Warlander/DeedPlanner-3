62b8ee7b474b4c723c6ba04ac5ab1ca17d9f6848
Maciej Cyranowicz <mackordian@gmail.com>
2023-08-27 13:27:24 +0200

    Improved tooltip behavior near screen edges

---
+++ Assets/Plugins/Warlander/ExtensionUtils/RectExtensionUtils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Warlander.ExtensionUtils
+{
+    public static class RectExtensionUtils
+    {
+        public static Rect ShiftPosition(this Rect rect, Vector2 shift)
+        {
+            return new Rect(rect.position + shift, rect.size);
+        }
+
+        /// <summary>
+        /// Origin is (0, 0).
+        /// </summary>
+        public static Rect ShiftToOrigin(this Rect rect)
+        {
+            return rect.ShiftPosition(-rect.position);
+        }
+    }
+}
--- Assets/Warlander/Deedplanner/Gui/Tooltips/Tooltip.cs
+++ Assets/Warlander/Deedplanner/Gui/Tooltips/Tooltip.cs
@@ -2,7 +2,9 @@ using System;
 using TMPro;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 using Warlander.Deedplanner.Inputs;
+using Warlander.ExtensionUtils;
 using Warlander.UI.Windows;
 using Zenject;
 
@@ -14,39 +16,43 @@ namespace Warlander.Deedplanner.Gui.Tooltips
         
         [SerializeField] private TMP_Text text;
         [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private CanvasScaler _canvasScaler;
         [SerializeField] private RectTransform _referenceCanvasTransform;
         [SerializeField] private RectTransform _transformToMove;
         [SerializeField] private float animationSpeed = 10f;
+        [SerializeField] private Vector2 _cursorCorrection = new Vector2(0, -20);
 
+        private Vector2 _cursorCorrectionToUse;
+        
         public string Value
         {
             get => text.text;
             set
             {
-                if (string.IsNullOrEmpty(value))
+                bool empty = string.IsNullOrEmpty(value);
+
+                if (empty)
                 {
                     canvasGroup.DOKill();
-                    canvasGroup.DOFade(0, animationSpeed).SetEase(Ease.Linear).SetSpeedBased();
-                    return;
+                    canvasGroup.DOFade(0, animationSpeed).SetEase(Ease.Linear).SetSpeedBased()
+                        .OnComplete(() => gameObject.SetActive(false));
                 }
-                
-                canvasGroup.DOKill();
-                canvasGroup.DOFade(1, animationSpeed).SetEase(Ease.Linear).SetSpeedBased();
-                gameObject.SetActive(true);
-                text.text = value;
+                else
+                {
+                    canvasGroup.DOKill();
+                    canvasGroup.DOFade(1, animationSpeed).SetEase(Ease.Linear).SetSpeedBased();
+                    gameObject.SetActive(true);
+                    text.text = value;
+                }
             }
         }
 
         private void Awake()
         {
             canvasGroup.alpha = 0;
+            gameObject.SetActive(false);
         }
-
-        private void OnEnable()
-        {
-            UpdatePosition();
-        }
-
+        
         private void Update()
         {
             UpdatePosition();
@@ -55,10 +61,45 @@ namespace Warlander.Deedplanner.Gui.Tooltips
         private void UpdatePosition()
         {
             Vector2 focusPos = _input.MapInputShared.FocusPosition.ReadValue<Vector2>();
-            Vector2 cursorCorrection = new Vector2(0, -20);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(_referenceCanvasTransform, focusPos, null,
-                out Vector2 localPos);
-            _transformToMove.localPosition = localPos + cursorCorrection;
+            
+            Rect referenceCanvasRect = _referenceCanvasTransform.rect;
+            float widthRatio = _canvasScaler.referenceResolution.y / _canvasScaler.referenceResolution.x;
+            Vector2 tooltipSize = _transformToMove.sizeDelta;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_referenceCanvasTransform,
+                focusPos.AddX(tooltipSize.x * widthRatio), null, out Vector2 tooltipRightmostPoint);
+            bool rightEdgeWithinBounds = referenceCanvasRect.Contains(tooltipRightmostPoint);
+            int pivotX = rightEdgeWithinBounds ? 0 : 1;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_referenceCanvasTransform,
+                focusPos.AddY(-tooltipSize.y), null, out Vector2 tooltipBottommostPoint);
+            bool bottomEdgeWithinBounds = referenceCanvasRect.Contains(tooltipBottommostPoint);
+            int pivotY = bottomEdgeWithinBounds ? 1 : 0;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _referenceCanvasTransform, focusPos, null, out Vector2 localPos);
+            
+            bool isPointerOnScreen = referenceCanvasRect.Contains(localPos);
+            
+            // Don't update pivot and connection if pointer goes off-screen - this will cause sudden tooltip shift otherwise.
+            if (isPointerOnScreen)
+            {
+                _transformToMove.pivot = new Vector2(pivotX, pivotY);
+                
+                Vector2 finalCursorCorrection = _cursorCorrection;
+                if (rightEdgeWithinBounds == false)
+                {
+                    finalCursorCorrection = finalCursorCorrection.SetX(-finalCursorCorrection.x);
+                }
+                if (bottomEdgeWithinBounds == false)
+                {
+                    finalCursorCorrection = finalCursorCorrection.SetY(-finalCursorCorrection.y);
+                }
+
+                _cursorCorrectionToUse = finalCursorCorrection;
+            }
+
+            _transformToMove.localPosition = localPos + _cursorCorrectionToUse;
         }
 
         private void OnDestroy()
