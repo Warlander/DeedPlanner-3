0028676566b03975c0b61af37b5d7a8af3c98df4
Maciej Cyranowicz <mackordian@gmail.com>
2023-09-05 19:37:12 +0200

    Added basic key rebinding support

--- Assets/Warlander/Deedplanner/Gui/Widgets/InputSettingElement.cs
+++ Assets/Warlander/Deedplanner/Gui/Widgets/InputSettingElement.cs
@@ -9,11 +9,37 @@ namespace Warlander.Deedplanner.Gui.Widgets
     {
         [SerializeField] private TMP_Text _actionNameText;
         [SerializeField] private TMP_Text _currentKeybindText;
+        [SerializeField] private Button _changeBindingButton;
+
+        private InputAction _inputAction;
+        
+        private void Awake()
+        {
+            _changeBindingButton.onClick.AddListener(OnChangeBindingClick);
+        }
         
         public void Set(InputAction inputAction)
         {
+            _inputAction = inputAction;
+            
             _actionNameText.text = inputAction.name;
             _currentKeybindText.text = inputAction.GetBindingDisplayString();
+        }
+
+        private void OnChangeBindingClick()
+        {
+            _inputAction.Disable();
+            
+           _inputAction.PerformInteractiveRebinding().OnComplete(operation =>
+           {
+               _inputAction.Enable();
+               Set(_inputAction);
+               operation.Dispose();
+           }).OnCancel(operation =>
+           {
+               _inputAction.Enable();
+               operation.Dispose();
+           }).Start();
         }
     }
 }
