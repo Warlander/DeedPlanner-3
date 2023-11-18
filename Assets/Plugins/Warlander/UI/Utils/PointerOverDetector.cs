dc603b58a2f99d83b9e5a419dd67db773b66c87d
Maciej Cyranowicz <mackordian@gmail.com>
2023-11-18 02:07:04 +0100

    Added PointerOverDetector shared script

---
+++ Assets/Plugins/Warlander/UI/Utils/PointerOverDetector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Warlander.UI.Utils
+{
+    public class PointerOverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public bool IsPointerOver { get; private set; }
+
+        private DateTime _pointerEnterTime;
+
+        public TimeSpan GetPointerOverDuration()
+        {
+            if (IsPointerOver == false)
+            {
+                return TimeSpan.Zero;
+            }
+
+            return DateTime.UtcNow - _pointerEnterTime;
+        }
+        
+        void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+        {
+            IsPointerOver = true;
+            _pointerEnterTime = DateTime.UtcNow;
+        }
+
+        void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+        {
+            IsPointerOver = false;
+        }
+    }
+}
