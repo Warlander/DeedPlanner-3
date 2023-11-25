b70ff4a6a5f0469c3dbd33f8b80a6113bf611d2e
Maciej Cyranowicz <mackordian@gmail.com>
2023-11-25 17:07:24 +0100

    Added SwitchObjectOnToggle UI utility script

---
+++ Assets/Plugins/Warlander/UI/Utils/SwitchObjectOnToggle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Warlander.UI.Utils
+{
+    public class SwitchObjectOnToggle : MonoBehaviour
+    {
+        [SerializeField] private Toggle _trackedToggle;
+        [SerializeField] private SwitchObjectOnToggleState _showWhen = SwitchObjectOnToggleState.On;
+
+        private void Awake()
+        {
+            _trackedToggle.onValueChanged.AddListener(TrackedToggleOnValueChanged);
+            
+            UpdateState();
+        }
+
+        private void TrackedToggleOnValueChanged(bool newState)
+        {
+            UpdateState();
+        }
+        
+        private void UpdateState()
+        {
+            if (_trackedToggle)
+            {
+                bool reverse = _showWhen == SwitchObjectOnToggleState.Off;
+                if (reverse)
+                {
+                    gameObject.SetActive(_trackedToggle.isOn == false);
+                }
+                else
+                {
+                    gameObject.SetActive(_trackedToggle.isOn);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_trackedToggle)
+            {
+                _trackedToggle.onValueChanged.RemoveListener(TrackedToggleOnValueChanged);
+            }
+        }
+    }
+
+    public enum SwitchObjectOnToggleState
+    {
+        On, Off
+    }
+}
