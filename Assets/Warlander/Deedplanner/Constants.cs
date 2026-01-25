df08a5964a5ee0a0c0b1942c9dc1e3727b1fca17
Warlander <mackordian@gmail.com>
2026-01-25 04:15:37 +0100

    Version bump to DP 3.2.1 in constants

--- Assets/Warlander/Deedplanner/Constants.cs
+++ Assets/Warlander/Deedplanner/Constants.cs
@@ -3,7 +3,7 @@
 
     public static class Constants
     {
-        public const string VersionString = "3.2.0 ALPHA";
+        public const string VersionString = "3.2.1 ALPHA";
         public const string TitleString = "DeedPlanner " + VersionString;
         public const string SimpleTitleString = "DeedPlanner";
         public const int SteamAppId = 1179760;
