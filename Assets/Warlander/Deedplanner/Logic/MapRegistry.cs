0d9046fc3622d375a65cb863c3c9a2f03c2c13ff
Warlander <mackordian@gmail.com>
2026-08-08 23:56:26 +0200

    Revert "Destroy escaped map instances in MapRegistry.SetMap (zombie ground plane)"
    This reverts commit 256ca2f41390bf4e5ca1aca7538ea4a3d4a2660c.

--- Assets/Warlander/Deedplanner/Logic/MapRegistry.cs
+++ Assets/Warlander/Deedplanner/Logic/MapRegistry.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEngine;
 using Warlander.Deedplanner.Data;
 
 namespace Warlander.Deedplanner.Logic
@@ -13,17 +12,6 @@ namespace Warlander.Deedplanner.Logic
         public void SetMap(Map map)
         {
             CurrentMap = map;
-
-            // safety net: destroy any map instances that escaped the normal unload path
-            foreach (Map other in UnityEngine.Object.FindObjectsByType<Map>(FindObjectsSortMode.None))
-            {
-                if (other != map)
-                {
-                    other.gameObject.SetActive(false);
-                    UnityEngine.Object.Destroy(other.gameObject);
-                }
-            }
-
             MapInitialized?.Invoke();
         }
     }
