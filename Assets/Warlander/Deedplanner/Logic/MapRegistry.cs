256ca2f41390bf4e5ca1aca7538ea4a3d4a2660c
Warlander <mackordian@gmail.com>
2026-08-08 23:50:47 +0200

    Destroy escaped map instances in MapRegistry.SetMap (zombie ground plane)

--- Assets/Warlander/Deedplanner/Logic/MapRegistry.cs
+++ Assets/Warlander/Deedplanner/Logic/MapRegistry.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 using Warlander.Deedplanner.Data;
 
 namespace Warlander.Deedplanner.Logic
@@ -12,6 +13,17 @@ namespace Warlander.Deedplanner.Logic
         public void SetMap(Map map)
         {
             CurrentMap = map;
+
+            // safety net: destroy any map instances that escaped the normal unload path
+            foreach (Map other in UnityEngine.Object.FindObjectsByType<Map>(FindObjectsSortMode.None))
+            {
+                if (other != map)
+                {
+                    other.gameObject.SetActive(false);
+                    UnityEngine.Object.Destroy(other.gameObject);
+                }
+            }
+
             MapInitialized?.Invoke();
         }
     }
