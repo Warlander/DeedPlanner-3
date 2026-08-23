f2704637e5646edf7cf23b4320c9e98696a9fc0e
Warlander <mackordian@gmail.com>
2026-08-23 16:57:08 +0200

    Added dock drag-paint interaction with commands and validity rules

---
+++ Assets/Warlander/Deedplanner/Data/Docks/DockRemovalCommand.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Warlander.Deedplanner.Utils;
+
+namespace Warlander.Deedplanner.Data.Docks
+{
+    public class DockRemovalCommand : IReversibleCommand
+    {
+        private readonly Map _map;
+        private readonly Dock _dock;
+
+        public DockRemovalCommand(Map map, Dock dock)
+        {
+            _map = map;
+            _dock = dock;
+        }
+
+        public void Execute()
+        {
+            _map.RemoveDock(_dock);
+            _dock.Tile.UnregisterDock();
+            _dock.gameObject.SetActive(false);
+        }
+
+        public void Undo()
+        {
+            _dock.Tile.RegisterDock(_dock);
+            _map.AddDock(_dock);
+            _dock.gameObject.SetActive(true);
+        }
+
+        public void DisposeUndo()
+        {
+            Object.Destroy(_dock.gameObject);
+        }
+
+        public void DisposeRedo()
+        {
+        }
+    }
+}
--- Assets/Warlander/Deedplanner/Updaters/FloorUpdater.cs
+++ Assets/Warlander/Deedplanner/Updaters/FloorUpdater.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Warlander.Deedplanner.Data;
 using Warlander.Deedplanner.Data.Docks;
@@ -12,12 +13,18 @@ namespace Warlander.Deedplanner.Updaters
 {
     public class FloorUpdater : IUpdater
     {
+        private enum DockStroke
+        {
+            None, Paint, Erase
+        }
+
         private readonly IFloorUpdaterView _view;
         private readonly TooltipHandler _tooltipHandler;
         private readonly CameraCoordinator _cameraCoordinator;
         private readonly DPInput _input;
         private readonly MapHandler _mapHandler;
         private readonly TabContext _tabContext;
+        private readonly DockFactory _dockFactory;
 
         public Tab TargetTab => Tab.Floors;
 
@@ -26,13 +33,24 @@ namespace Warlander.Deedplanner.Updaters
         private FloorPaintMode _paintMode = FloorPaintMode.Floors;
         private bool _dockSupportAuto = true;
         private DockSupportData _selectedDockSupport;
+        private DockSupportData _lastPillarSupport;
+
+        private DockStroke _dockStroke = DockStroke.None;
+        private Tile _anchorTile;
+        private Floor _anchorFloor;
+        private int _strokeHeight;
+        private bool _strokeConfirmed;
+        private Tile _lastStrokeTile;
+        private Tile _previousPaintedTile;
+        private readonly HashSet<Tile> _paintedTiles = new HashSet<Tile>();
+        private readonly List<Dock> _invalidMarkers = new List<Dock>();
 
         public FloorPaintMode PaintMode => _paintMode;
         public bool DockSupportAuto => _dockSupportAuto;
         public DockSupportData SelectedDockSupport => _selectedDockSupport;
 
         public FloorUpdater(IFloorUpdaterView view, TooltipHandler tooltipHandler, CameraCoordinator cameraCoordinator,
-            DPInput input, MapHandler mapHandler, TabContext tabContext)
+            DPInput input, MapHandler mapHandler, TabContext tabContext, DockFactory dockFactory)
         {
             _view = view;
             _tooltipHandler = tooltipHandler;
@@ -40,6 +58,7 @@ namespace Warlander.Deedplanner.Updaters
             _input = input;
             _mapHandler = mapHandler;
             _tabContext = tabContext;
+            _dockFactory = dockFactory;
         }
 
         public void Initialize()
@@ -58,6 +77,7 @@ namespace Warlander.Deedplanner.Updaters
             }
 
             _view.PushSelection();
+            _lastPillarSupport = Database.DockSupports["dwp"];
         }
 
         public void Enable()
@@ -65,7 +85,10 @@ namespace Warlander.Deedplanner.Updaters
             _tabContext.TileSelectionMode = TileSelectionMode.Tiles;
         }
 
-        public void Disable() { }
+        public void Disable()
+        {
+            ResetDockStroke();
+        }
 
         private void OnFloorSelected(FloorData data)
         {
@@ -81,12 +104,17 @@ namespace Warlander.Deedplanner.Updaters
         {
             _paintMode = mode;
             _view.SetDockSupportSectionVisible(mode == FloorPaintMode.Docks);
+            ResetDockStroke();
         }
 
         private void OnDockSupportChanged(bool auto, DockSupportData support)
         {
             _dockSupportAuto = auto;
             _selectedDockSupport = support;
+            if (support != null && (support.Type == DockSupportType.WoodPillar || support.Type == DockSupportType.StonePillar))
+            {
+                _lastPillarSupport = support;
+            }
         }
 
         public void Tick()
@@ -96,9 +124,9 @@ namespace Warlander.Deedplanner.Updaters
                 _mapHandler.Map.CommandManager.FinishAction();
             }
 
-            // Dock painting lands with the drag-paint interaction; floor painting stays out of dock mode.
             if (_paintMode == FloorPaintMode.Docks)
             {
+                TickDockMode();
                 return;
             }
 
@@ -147,6 +175,227 @@ namespace Warlander.Deedplanner.Updaters
             {
                 _mapHandler.Map[x, y].SetFloor(null, _orientation, floor);
             }
+        }
+
+        private void TickDockMode()
+        {
+            Map map = _mapHandler.Map;
+
+            if (_input.UpdatersShared.Placement.WasReleasedThisFrame() || _input.UpdatersShared.Deletion.WasReleasedThisFrame())
+            {
+                ResetDockStroke();
+                return;
+            }
+
+            RaycastHit raycast = _cameraCoordinator.Current.CurrentRaycast;
+            if (!raycast.transform)
+            {
+                return;
+            }
+
+            int x = Mathf.FloorToInt(raycast.point.x / 4f);
+            int y = Mathf.FloorToInt(raycast.point.z / 4f);
+            if (x < 0 || y < 0 || x >= map.Width || y >= map.Height)
+            {
+                return;
+            }
+
+            Tile tile = map[x, y];
+
+            bool placementHeld = _input.UpdatersShared.Placement.ReadValue<float>() > 0;
+            bool deletionHeld = _input.UpdatersShared.Deletion.ReadValue<float>() > 0;
+
+            if (_dockStroke == DockStroke.None)
+            {
+                if (_input.UpdatersShared.Placement.WasPressedThisFrame())
+                {
+                    TryStartPaintStroke(raycast, tile);
+                }
+                else if (_input.UpdatersShared.Deletion.WasPressedThisFrame())
+                {
+                    _dockStroke = DockStroke.Erase;
+                    _lastStrokeTile = null;
+                }
+                return;
+            }
+
+            if (_dockStroke == DockStroke.Paint && placementHeld)
+            {
+                _tooltipHandler.ShowTooltipText("h " + _strokeHeight);
+                if (tile != _lastStrokeTile)
+                {
+                    _lastStrokeTile = tile;
+                    if (!_paintedTiles.Contains(tile))
+                    {
+                        TryPaintTile(tile, false);
+                    }
+                }
+            }
+            else if (_dockStroke == DockStroke.Erase && deletionHeld)
+            {
+                if (tile != _lastStrokeTile)
+                {
+                    _lastStrokeTile = tile;
+                    Dock dock = tile.Dock;
+                    if (dock != null)
+                    {
+                        map.CommandManager.AddToActionAndExecute(new DockRemovalCommand(map, dock));
+                    }
+                }
+            }
+        }
+
+        private void TryStartPaintStroke(RaycastHit raycast, Tile tile)
+        {
+            Dock hitDock = raycast.transform.GetComponent<Dock>();
+            Floor hitFloor = raycast.transform.GetComponent<Floor>();
+
+            if (hitDock != null && hitDock.Tile != null)
+            {
+                _strokeHeight = hitDock.Height;
+                _anchorFloor = null;
+            }
+            else if (hitFloor != null && hitFloor.Valid && hitFloor.Level >= 0)
+            {
+                _strokeHeight = tile.SurfaceHeight + hitFloor.Level * 30;
+                _anchorFloor = hitFloor;
+            }
+            else
+            {
+                _tooltipHandler.ShowTooltipText("<color=red><b>Docks must start from an existing floor or dock</b></color>");
+                return;
+            }
+
+            _dockStroke = DockStroke.Paint;
+            _anchorTile = tile;
+            _strokeConfirmed = false;
+            _lastStrokeTile = tile;
+            _previousPaintedTile = null;
+            _paintedTiles.Clear();
+        }
+
+        private void TryPaintTile(Tile tile, bool isAnchor)
+        {
+            Map map = _mapHandler.Map;
+
+            if (!isAnchor)
+            {
+                if (tile == _anchorTile && !_strokeConfirmed)
+                {
+                    return;
+                }
+
+                DockHardBlock block = DockSupportResolver.GetHardBlock(map, tile.X, tile.Y, _strokeHeight);
+                if (block != DockHardBlock.None)
+                {
+                    CreateInvalidMarker(tile);
+                    _paintedTiles.Add(tile);
+                    return;
+                }
+
+                if (!_strokeConfirmed)
+                {
+                    ConvertAnchor();
+                }
+            }
+
+            DockSupportData support = ResolveSupport(map, tile, out EntityOrientation braceDir);
+            Dock replacedDock = tile.Dock;
+            Dock newDock = _dockFactory.CreateDock(map, tile.X, tile.Y, _strokeHeight, _selectedFloor, support, braceDir);
+            map.CommandManager.AddToActionAndExecute(new DockPlacementCommand(map, newDock, replacedDock));
+            _paintedTiles.Add(tile);
+            _previousPaintedTile = tile;
+        }
+
+        private void ConvertAnchor()
+        {
+            if (_anchorFloor != null)
+            {
+                _anchorFloor.Tile.SetFloor(null, _orientation, _anchorFloor.Level);
+            }
+
+            _strokeConfirmed = true;
+            TryPaintTile(_anchorTile, true);
+        }
+
+        private DockSupportData ResolveSupport(Map map, Tile tile, out EntityOrientation braceDir)
+        {
+            if (_dockSupportAuto)
+            {
+                return DockSupportResolver.ResolveAutoSupport(map, tile.X, tile.Y, _strokeHeight,
+                    _lastPillarSupport, out braceDir);
+            }
+
+            DockSupportData support = _selectedDockSupport;
+            if (support != null && support.Type == DockSupportType.Brace)
+            {
+                DockSupportResolver.TryPickBraceSide(map, tile.X, tile.Y, _strokeHeight, _previousPaintedTile,
+                    out braceDir);
+            }
+            else
+            {
+                braceDir = EntityOrientation.Up;
+            }
+
+            return support;
+        }
+
+        private void CreateInvalidMarker(Tile tile)
+        {
+            GameObject markerObject = new GameObject("Invalid Dock Marker", typeof(Dock));
+            Dock marker = markerObject.GetComponent<Dock>();
+            marker.Initialize(tile, _strokeHeight, _selectedFloor, null, EntityOrientation.Up);
+
+            BoxCollider markerCollider = markerObject.GetComponent<BoxCollider>();
+            if (markerCollider)
+            {
+                markerCollider.enabled = false;
+            }
+
+            marker.ModelLoaded += OnMarkerModelLoaded;
+            if (marker.Model)
+            {
+                TintMarker(marker.Model);
+            }
+
+            _invalidMarkers.Add(marker);
+        }
+
+        private void OnMarkerModelLoaded(DynamicModelBehaviour behaviour, GameObject model)
+        {
+            TintMarker(model);
+        }
+
+        private static void TintMarker(GameObject model)
+        {
+            MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+            Color tint = new Color(1f, 0.15f, 0.15f, 0.5f);
+            foreach (Renderer childRenderer in model.GetComponentsInChildren<Renderer>())
+            {
+                childRenderer.GetPropertyBlock(propertyBlock);
+                propertyBlock.SetColor(ShaderPropertyIds.BaseColor, tint);
+                childRenderer.SetPropertyBlock(propertyBlock);
+            }
+        }
+
+        private void ResetDockStroke()
+        {
+            foreach (Dock marker in _invalidMarkers)
+            {
+                if (marker)
+                {
+                    Object.Destroy(marker.gameObject);
+                }
+            }
+            _invalidMarkers.Clear();
+
+            _dockStroke = DockStroke.None;
+            _anchorTile = null;
+            _anchorFloor = null;
+            _strokeConfirmed = false;
+            _lastStrokeTile = null;
+            _previousPaintedTile = null;
+            _paintedTiles.Clear();
         }
     }
 }
